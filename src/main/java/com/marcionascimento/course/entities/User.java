9cece682e6797edbddcdc981f0e04d9102c6bf08
marcionascimentoweb <mlrnrj2011@hotmailcom>
2022-01-26 18:19:18 -0300

    H2 database, test profile, JPA

--- src/main/java/com/marcionascimento/course/entities/User.java
+++ src/main/java/com/marcionascimento/course/entities/User.java
@@ -2,9 +2,17 @@ package com.marcionascimento.course.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 
+	@Id //Chave Primaria
+	@GeneratedValue(strategy = GenerationType.IDENTITY)//auto incremento
 	private Long id;
 	private String name;
 	private String email;
