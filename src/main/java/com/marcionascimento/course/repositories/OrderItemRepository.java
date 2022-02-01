31c055b07a2033d977220495f24172d18f9c4874
marcionascimentoweb <mlrnrj2011@hotmailcom>
2022-02-01 19:11:24 -0300

    OrderItem, many-to-may association with extra attributes PART 2

---
+++ src/main/java/com/marcionascimento/course/repositories/OrderItemRepository.java
@@ -0,0 +1,10 @@
+package com.marcionascimento.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.marcionascimento.course.entities.OrderItem;
+
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+	
+
+}
