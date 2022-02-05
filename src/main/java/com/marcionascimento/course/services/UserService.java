fd251407b1ffe897c3c744c1880ca6e286ac7bc6
marcionascimentoweb <mlrnrj2011@hotmailcom>
2022-02-04 23:12:27 -0300

    Exception handling - delete

--- src/main/java/com/marcionascimento/course/services/UserService.java
+++ src/main/java/com/marcionascimento/course/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.marcionascimento.course.entities.User;
 import com.marcionascimento.course.repositories.UserRepository;
+import com.marcionascimento.course.services.exceptions.DatabaseException;
 import com.marcionascimento.course.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -34,8 +37,13 @@ public class UserService {
 	
 	//Deletar
 	public void delete(Long id) {
+		try {
 		repository.deleteById(id);
-		
+		}catch (EmptyResultDataAccessException e) { //Error não encontrado
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) { //Error não pode deletar, pois tem pedidos associados
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	// Atualizar
